a30ff15dcf520a9ccfbbdd56eed2fb147561f141
AbdulAleemWorks <abdulaleemworks@github.com>
2026-01-26 20:54:00 +0500

    Q2

---
+++ Q2.js
@@ -0,0 +1,34 @@
+/*
+At the start of each game turn, you are given the height of the 8 mountains from left to right.
+By the end of the game turn, you must fire on the highest mountain by outputting its index (from 0 to 7).
+
+Firing on a mountain will only destroy part of it, reducing its height. Your ship descends after each pass.  
+*/
+
+/**
+ * The while loop represents the game.
+ * Each iteration represents a turn of the game
+ * where you are given inputs (the heights of the mountains)
+ * and where you have to print an output (the index of the mountain to fire on)
+ * The inputs you are given are automatically updated according to your last actions.
+ **/
+
+
+// game loop
+while (true) {
+    var highest=0 ;
+    var index=0;
+    for (let i = 0; i < 8; i++) {
+        const mountainH = parseInt(readline()); // represents the height of one mountain.
+        if(mountainH>highest){
+            highest=mountainH;
+            index=i
+        }
+    }
+
+    // Write an action using console.log()
+    // To debug: console.error('Debug messages...');
+
+    console.log(index);     // The index of the mountain to fire on.
+
+}
